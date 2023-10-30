8458f039a7616e9029d083086f1fd8c4279806d1
Nurunnahar <nurunnahar9081@gmail.com>
2023-10-30 16:53:27 +0600

    Home Slider Done

--- apple_shop/resources/views/component/HeroSlider.blade.php
+++ apple_shop/resources/views/component/HeroSlider.blade.php
@@ -1,51 +1,8 @@
 <div class="banner_section slide_medium shop_banner_slider staggered-animation-wrap">
     <div id="carouselExampleControls" class="carousel slide carousel-fade light_arrow" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item background_bg active" data-img-src="assets/images/banner16.jpg" style="background-image: url(&quot;assets/images/banner16.jpg&quot;);">
-                <div class="banner_slide_content banner_content_inner">
-                	<div class="container">
-                    	<div class="row">
-                            <div class="col-lg-7 col-10">
-                                <div class="banner_content overflow-hidden">
-                                    <h2 class="staggered-animation animated slideInRight" data-animation="slideInRight" data-animation-delay="0.5s" style="animation-delay: 0.5s; opacity: 1;">LED 75 INCH F58</h2>
-                                    <h5 class="mb-3 mb-sm-4 staggered-animation font-weight-light animated slideInRight" data-animation="slideInRight" data-animation-delay="1s" style="animation-delay: 1s; opacity: 1;">Get up to <span class="text_default">50%</span> off Today Only!</h5>
-                                    <a class="btn btn-fill-out staggered-animation text-uppercase animated slideInRight" href="shop-left-sidebar.html" data-animation="slideInRight" data-animation-delay="1.5s" style="animation-delay: 1.5s; opacity: 1;">Shop Now</a>
-                                </div>
-                            </div>
-                    	</div>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item background_bg" data-img-src="assets/images/banner17.jpg" style="background-image: url(&quot;assets/images/banner17.jpg&quot;);">
-                <div class="banner_slide_content banner_content_inner">
-                	<div class="container">
-                    	<div class="row">
-                    		<div class="col-lg-8 col-10">
-                                <div class="banner_content overflow-hidden">
-                                    <h2 class="staggered-animation animated slideInRight" data-animation="slideInRight" data-animation-delay="0.5s" style="animation-delay: 0.5s; opacity: 1;">Smart Phones</h2>
-                                    <h5 class="mb-3 mb-sm-4 staggered-animation font-weight-light animated slideInRight" data-animation="slideInRight" data-animation-delay="1s" style="animation-delay: 1s; opacity: 1;"><span class="text_default">50%</span> off in all products</h5>
-                                    <a class="btn btn-fill-out staggered-animation text-uppercase animated slideInRight" href="shop-left-sidebar.html" data-animation="slideInRight" data-animation-delay="1.5s" style="animation-delay: 1.5s; opacity: 1;">Shop Now</a>
-                                </div>
-                            </div>
-                    	</div>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item background_bg" data-img-src="assets/images/banner18.jpg" style="background-image: url(&quot;assets/images/banner18.jpg&quot;);">
-                <div class="banner_slide_content banner_content_inner">
-                	<div class="container">
-                    	<div class="row">
-                    		<div class="col-lg-8 col-10">
-                                <div class="banner_content overflow-hidden">
-                                    <h2 class="staggered-animation animated slideInRight" data-animation="slideInRight" data-animation-delay="0.5s" style="animation-delay: 0.5s; opacity: 1;">Beat Headphone</h2>
-                                    <h5 class="mb-3 mb-sm-4 staggered-animation font-weight-light animated slideInRight" data-animation="slideInRight" data-animation-delay="1s" style="animation-delay: 1s; opacity: 1;">Taking your Viewing Experience to Next Level</h5>
-                                    <a class="btn btn-fill-out staggered-animation text-uppercase animated slideInRight" href="shop-left-sidebar.html" data-animation="slideInRight" data-animation-delay="1.5s" style="animation-delay: 1.5s; opacity: 1;">Shop Now</a>
-                                </div>
-                            </div>
-                    	</div>
-                    </div>
-                </div>
-            </div>
+        <div id="carouselSection" class="carousel-inner">
+
+
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev"><i class="ion-chevron-left"></i></a>
         <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next"><i class="ion-chevron-right"></i></a>
@@ -53,3 +10,35 @@
 </div>
 
 
+
+
+
+<script>
+     Hero();
+    async function Hero() {
+        let res = await axios.get("/ListProductSlider");
+        $("#carouselSection").empty();
+        res.data['data'].forEach((item,i)=>{
+            let activeClass=''
+            if(i===0){
+                activeClass=' active '
+            }
+            let SliderItem=`<div class="carousel-item  background_bg ${activeClass}" style="background-image: url('${item['image']}')">
+                <div class="banner_slide_content">
+                    <div class="container"><!-- STRART CONTAINER -->
+                        <div class="row">
+                            <div class="col-lg-7 col-9">
+                                <div class="banner_content overflow-hidden">
+                                    <h5 class="mb-3 staggered-animation font-weight-light" data-animation="slideInLeft" data-animation-delay="0.5s">${item['price']}</h5>
+                                    <h2 class="staggered-animation" data-animation="slideInLeft" data-animation-delay="1s">${item['title']}</h2>
+                                    <a class="btn btn-fill-out rounded-0 staggered-animation text-uppercase" href="#" data-animation="slideInLeft" data-animation-delay="1.5s">Shop Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- END CONTAINER-->
+                </div>
+            </div>`
+            $("#carouselSection").append(SliderItem)
+        })
+    }
+</script>
