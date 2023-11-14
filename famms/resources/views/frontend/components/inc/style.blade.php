272da4c5fc66a99d2fb87e46b8a9c6aada6e7f16
Nurunnahar <nurunnahar9081@gmail.com>
2023-11-14 18:53:38 +0600

    frontend html done

---
+++ famms/resources/views/frontend/components/inc/style.blade.php
@@ -0,0 +1,9 @@
+<link rel="shortcut icon" href="{{ asset('frontend/images/favicon.png') }}" type="">
+<!-- bootstrap core css -->
+<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/bootstrap.css') }}" />
+<!-- font awesome style -->
+<link href="{{ asset('frontend/css/font-awesome.min.css') }}" rel="stylesheet" />
+<!-- Custom styles for this template -->
+<link href="{{ asset('frontend/css/style.css') }}" rel="stylesheet" />
+<!-- responsive style -->
+<link href="{{ asset('frontend/css/responsive.css') }}" rel="stylesheet" />
---
+++ famms/resources/views/frontend/layout/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Famms - Fashion HTML Template</title>
+    @include('frontend.components.inc.style')
+</head>
+
+<body>
+    @include('frontend.components.hero')
+    @include('frontend.components.why_shop_with_us')
+
+    @include('frontend.components.arrival')
+
+
+
+    @include('frontend.components.subscribe')
+    @include('frontend.components.clients')
+    @include('frontend.components.footer')
+    <div class="cpy_">
+        <p class="mx-auto">© 2021 All Rights Reserved By <a href="https://html.design/">Free Html Templates</a><br>
+
+            Distributed By <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
+
+        </p>
+    </div>
+    @include('frontend.components.inc.script')
+</body>
+
+</html>
