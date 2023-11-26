82a03efe04d59c1d57f91e671f72b7dbf42d89c5
Nurunnahar <nurunnahar9081@gmail.com>
2023-11-26 22:28:08 +0600

    create admin mail  (hotel management system)

--- hotel_management_system/app/Http/Controllers/Admin/HomeController.php
+++ hotel_management_system/app/Http/Controllers/Admin/HomeController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
     function loginPage(){
+        $pass = Hash::make('1234');
+        dd($pass);
         return view('admin.components.login');
     }
     function forgetPasswordPage(){
