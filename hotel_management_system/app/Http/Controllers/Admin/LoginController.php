155a0ac37bae1ed46c325002fc5d9e4bd9e9a2c3
Nurunnahar <nurunnahar9081@gmail.com>
2023-11-29 12:52:38 +0600

    admin login with verify but AdminMiddleware does not work (hotel management system)

--- hotel_management_system/app/Http/Controllers/Admin/LoginController.php
+++ hotel_management_system/app/Http/Controllers/Admin/LoginController.php
@@ -7,31 +7,35 @@ use App\Mail\AdminMail;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 
 class LoginController extends Controller
 {
- function login(Request $request){
-   $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-   ]);
 
-   $credential =[
-        'email' => $request->email,
-        'password' => $request->password
-   ];
-   if(Auth::guard('admin')->attempt($credential)){
-        return redirect()->route('admin.dashboard');
-   }else{
-        return redirect()->route('admin.login')->with('error', 'Information is not Correct');
-   }
- }
+    function loginPage(){
 
- function loginPage(){
+        return view('admin.components.login');
+    }
+    
+    function login(Request $request){
+        $request->validate([
+             'email' => 'required|email',
+             'password' => 'required',
+        ]);
 
-    return view('admin.components.login');
-}
+        $credential =[
+             'email' => $request->email,
+             'password' => $request->password
+        ];
+        if(Auth::guard('admin')->attempt($credential)){
+             return redirect()->route('admin.dashboard');
+        }else{
+             return redirect()->route('admin.login')->with('error', 'Information is not Correct');
+        }
+      }
+
+
 
 
 function logout(){
@@ -43,23 +47,44 @@ function forgetPasswordPage(){
     return view('admin.components.forget_password');
 }
  function forgetPassword(Request $request){
-     $request->validate([
-        'email' => 'required|email',
-     ]);
-     $admin_data = Admin::where('email', $request->email)->first();
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+        $admin_data = Admin::where('email', $request->email)->first();
+        if(!$admin_data){
+            return redirect()->back()->with('error', 'Email address not found !!');
+        }
+        $token = Hash('sha256', time());
+        $admin_data->token = $token;
+        $admin_data->update();
+
+        $reset_link = url('admin/reset-password/'.$token.'/'.$request->email);
+        $message = 'Please click on this link: <br><a href="'.$reset_link.'">Click here</a>';
+        Mail::to($request->email)->send(new AdminMail($message));
+        return redirect()->route('admin.login')->with('success', 'Please check your email and follow the steps');
+
+}
+
+function resetPasswordPage($token,$email){
+     $admin_data = Admin::where('email', $email)->where('token', $token)->first();
      if(!$admin_data){
-        return redirect()->back()->with('error', 'Email address not found !!');
+        return redirect()->route('admin.login');
      }
-     $token = Hash('sha256', time());
-     $admin_data->token = $token;
-     $admin_data->update();
+     return view('admin.components.reset_password',compact('token','email'));
+}
 
-     $reset_link = url('admin/reset-password/'.$token.'/'.$request->email);
-     $message = 'Please click on this link: <br><a href="'.$reset_link.'">Click here</a>';
+function resetPassword(Request $request){
+    $request->validate([
+        'password' => 'required',
+        'retype_password' => 'required|same:password'
+    ]);
 
+    $admin_data = Admin::where('token',$request->token)->where('email',$request->email)->first();
 
-     Mail::to($request->email)->send(new AdminMail($message));
-     return redirect()->route('admin.login')->with('success', 'Please check your email and follow the steps');
+    $admin_data->password = Hash::make($request->password);
+    $admin_data->token = '';
+    $admin_data->update();
+    return redirect()->route('admin.login')->with('success', 'Password is reset successfully');
+}
 
- }
 }
--- hotel_management_system/routes/web.php
+++ hotel_management_system/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\LoginController;
@@ -14,15 +15,19 @@ Route::prefix('admin/')->group(function(){
     Route::get('logout',[LoginController::class, 'logout'])->name('admin.logout');
     Route::get('forgrt-password',[LoginController::class, 'forgetPasswordPage'])->name('forget.password.page');
     Route::post('forgrt-password',[LoginController::class, 'forgetPassword'])->name('forget.password');
+    Route::get('reset-password/{token}/{email}',[LoginController::class, 'resetPasswordPage'])->name('reset.password.page');
+    Route::post('reset-password',[LoginController::class, 'resetPassword'])->name('reset.password');
 
-    Route::get('dashboard',[HomeController::class, 'Dashboard'])->name('admin.dashboard')->middleware('admin');
-
-
-
-
-
-
-
-
-
+    // Route::get('dashboard',[HomeController::class, 'Dashboard'])->name('admin.dashboard') ;
+    // Route::get('dashboard',[HomeController::class, 'Dashboard'])->name('admin.dashboard')->middleware('admin');
+    Route::get('dashboard',[HomeController::class, 'Dashboard'])->name('admin.dashboard')->middleware(AdminMiddleware::class);
 });
+
+
+
+
+
+
+
+
+
