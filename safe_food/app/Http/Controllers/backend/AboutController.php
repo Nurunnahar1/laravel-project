0893aaad4cd55c01517db2da12b858f9b3e63c25
Nurunnahar <nurunnahar9081@gmail.com>
2023-12-14 12:15:01 +0600

    adout data update successfully (safe food)

--- safe_food/app/Http/Controllers/backend/AboutController.php
+++ safe_food/app/Http/Controllers/backend/AboutController.php
@@ -20,9 +20,7 @@ class AboutController extends Controller
             'text' => 'required|string',
             'designation' => 'required|string',
             'name' => "required|string",
-            'image' => "nullable|image|max:1024",
-            'signature_image' => "nullable|image|max:1024",
-            'about_image' => "nullable|image|max:1024",
+
 
         ]);
         $about = About::where('id', $id)->first();
@@ -30,45 +28,43 @@ class AboutController extends Controller
         // =====image=====
         if ($request->hasFile('image')) {
             $request->validate([
-                'image' => 'image|mimes:jpg,jpeg,png,gif,webp',
+                'image' => 'nullable|image|mimes:jpg,jpeg,png,gif,webp',
             ]);
-            unlink(public_path('uploads/about/image' . $about->image));
+            unlink(public_path('uploads/about/image/' . $about->image));
             $ext = $request->file('image')->extension();
             $final_name = time() . '.' . $ext;
             $request->file('image')->move(public_path('uploads/about/image'), $final_name);
             $about->image = $final_name;
         }
 
-        // =====signature_image=====
-        if ($request->hasFile('signature_image')) {
-            $request->validate([
-                'signature_image' => 'image|mimes:jpg,jpeg,png,gif,webp',
-            ]);
-            unlink(public_path('uploads/about/signature_image' . $about->signature_image));
-            $ext = $request->file('signature_image')->extension();
-            $final_name = time() . '.' . $ext;
-            $request->file('signature_image')->move(public_path('uploads/about/signature_image'), $final_name);
-            $about->signature_image = $final_name;
-        }
+           // =====signature image=====
+            if ($request->hasFile('signature')) {
+                $request->validate([
+                    'signature' => 'nullable|image|mimes:jpg,jpeg,png,gif,webp',
+                ]);
+                unlink(public_path('uploads/about/signature/' . $about->signature));
+                $ext = $request->file('signature')->extension();
+                $sfinal_name = time() . '.' . $ext;
+                $request->file('signature')->move(public_path('uploads/about/signature'), $sfinal_name);
+                $about->signature = $sfinal_name;
+            }
 
-        // =====about_image=====
-        if ($request->hasFile('about_image')) {
-            $request->validate([
-                'about_image' => 'image|mimes:jpg,jpeg,png,gif,webp',
-            ]);
-            unlink(public_path('uploads/about/about_image' . $about->about_image));
-            $ext = $request->file('about_image')->extension();
-            $final_name = time() . '.' . $ext;
-            $request->file('about_image')->move(public_path('uploads/about/about_image'), $final_name);
-            $about->about_image = $final_name;
-        }
+            // =====about image=====
+            if ($request->hasFile('about_image')) {
+                $request->validate([
+                    'about_image' => 'nullable|image|mimes:jpg,jpeg,png,gif,webp',
+                ]);
+                unlink(public_path('uploads/about/about/' . $about->about_image));
+                $ext = $request->file('about_image')->extension();
+                $afinal_name = time() . '.' . $ext;
+                $request->file('about_image')->move(public_path('uploads/about/about'), $afinal_name);
+                $about->about_image = $afinal_name;
+            }
 
         $about->heading = $request->heading;
         $about->text = $request->text;
         $about->name = $request->name;
         $about->designation = $request->designation;
-
-        // Update image properties before calling update
         $about->update();
 
         toastr()->success('About data updated successfully');
